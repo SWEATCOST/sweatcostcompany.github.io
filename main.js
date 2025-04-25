695c93e3ef2e54802070756d065143b555796fd8
SWEATCOST <sweatcost@gmail.com>
2025-04-26 00:09:59 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,6 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+import { Web3Modal }        from "@web3modal/standalone";
+import { EthereumClient, w3mConnectors, w3mProvider } from "@web3modal/ethereum";
+import { configureChains, createClient } from "wagmi";
+import { bsc }              from "@wagmi/chains";
+import { ethers }           from "ethers";
+// … 여러분의 DApp 초기화/이벤트 코드 …
