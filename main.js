a204a86363475420ede6cc29c52a95116a12cf6f
SWEATCOST <sweatcost@gmail.com>
2025-04-26 00:12:22 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-import { Web3Modal }        from "@web3modal/standalone";
-import { EthereumClient, w3mConnectors, w3mProvider } from "@web3modal/ethereum";
+// src/main.js
+import { Web3Modal }            from "@web3modal/standalone";
+import { EthereumClient }       from "@web3modal/ethereum";
+import { w3mConnectors, w3mProvider } from "@web3modal/ethereum";
 import { configureChains, createClient } from "wagmi";
-import { bsc }              from "@wagmi/chains";
-import { ethers }           from "ethers";
-// … 여러분의 DApp 초기화/이벤트 코드 …
+import { bsc }                  from "@wagmi/chains";
+import { ethers }               from "ethers";
+
+// (나머지 기존 DApp 초기화/이벤트 핸들러 코드)
